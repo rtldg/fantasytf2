9883602e1d63c85f1ff242a18873c72488e8cf34
Joe <55846624+rtldg@users.noreply.github.com>
2020-10-04 10:58:11 +0000

    Create fantasytf2.py

---
+++ fantasytf2.py
@@ -0,0 +1,74 @@
+"""
+Copyright 2020, rtldg <https://github.com/rtldg>
+Copying and distribution of this file, with or without modification, are permitted in any medium without royalty, provided the copyright notice and this notice are preserved. This file is offered as-is, without any warranty.
+"""
+
+import a2s # https://github.com/Yepoleb/python-a2s
+import sqlite3
+import time    # for time.time()
+from datetime import datetime # datetime.isoformat() & datetime.utcnow()
+import socket  # for socket.timeout
+
+addresses = [
+	("us1.uncledane.com", 27015), # Los Angeles
+	("us2.uncledane.com", 27015), # Chicago
+	("us3.uncledane.com", 27015), # San Francisco
+	("us4.uncledane.com", 27015), # New York City
+	("eu1.uncledane.com", 27015), # Frankfurt
+	("eu2.uncledane.com", 27015), # Berlin
+]
+
+conn = sqlite3.connect("fantasytf2.db")
+cursor = conn.cursor()
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS playerinfo
+	(datetime real, name text, score int, duration real, map text, address text)
+''')
+
+def get_server_data(address):
+	players = None
+	serverinfo = None
+	for x in range(2):
+		try:
+			players = a2s.players(address)
+			break
+		except: # probably socket.timeout...
+			pass
+	if players:
+		for x in range(2):
+			try:
+				serverinfo = a2s.info(address)
+				break
+			except: # probably socket.timeout...
+				pass
+	return (players, serverinfo)
+
+def grab_all_servers():
+	for address in addresses:
+		(players, serverinfo) = get_server_data(address)
+		if not players or not serverinfo:
+			print("[{}] {} skipped".format(
+				datetime.isoformat(datetime.utcnow()),
+				address
+			))
+			continue # skip I guess...
+		now = time.time()
+		fuckaddress = "{}:{}".format(address[0], address[1])
+		players = list(map(
+			lambda x: (now, x.name, x.score, x.duration, serverinfo.map_name, fuckaddress),
+			filter(lambda x: x.name != "", players)
+		))
+		cursor.executemany('INSERT INTO playerinfo VALUES (?,?,?,?,?,?)', players)
+		conn.commit()
+		print("[{}] {} retrieved {} players".format(
+			datetime.isoformat(datetime.utcnow()),
+			address,
+			len(players)
+		))
+		time.sleep(0.1)
+
+while True:
+	grab_all_servers()
+	time.sleep(13.0) # bad luck number
+
+#conn.close()
